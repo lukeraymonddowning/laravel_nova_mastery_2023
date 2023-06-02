84d2d04d118d1863f4d07ba9b80066cb4cfe33bf
Luke Downing <lukeraymonddowning@gmail.com>
2023-06-02 10:13:11 +0100

    Ep. 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue"],
+  content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./public/**/*.svg"
+  ],
   theme: {
     extend: {},
   },
