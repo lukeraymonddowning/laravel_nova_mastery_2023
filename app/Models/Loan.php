e6e3db9486ea6fb5733f579809fc5283fae3d486
Luke Downing <lukeraymonddowning@gmail.com>
2023-06-28 09:40:00 +0100

    Ep. 16 start

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -8,6 +8,8 @@ class Loan extends Pivot
 {
     public $incrementing = true;
 
+    protected $table = 'book_customer';
+
     protected $casts = [
         'due_back_at' => 'datetime',
         'returned_at' => 'datetime',
