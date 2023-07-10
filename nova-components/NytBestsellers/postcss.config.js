30f18235f9f89241d3d8841f1a555c9b9fbe8626
Luke Downing <lukeraymonddowning@gmail.com>
2023-07-10 12:13:11 +0100

    Ep. 24

--- nova-components/NytBestsellers/postcss.config.js
+++ nova-components/NytBestsellers/postcss.config.js
@@ -1,6 +1,13 @@
+const path = require("path");
+
 module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
+        uniquestyles: {
+            paths: [
+                path.join(__dirname, "../../public/vendor/nova/app.css"),
+            ]
+        },
     }
 }
